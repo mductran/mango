4346ac667645b07e1f04d8abf72bb82d26ba7fd6
mductran <ductran032118@gmail.com>
2024-03-08 01:07:41 +0700

    add hashing functions

--- go.mod
+++ go.mod
@@ -4,29 +4,28 @@ go 1.21.6
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/buke/quickjs-go v0.4.0
 	github.com/schollz/progressbar/v3 v3.14.1
 	go.mongodb.org/mongo-driver v1.13.1
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/buke/quickjs-go v0.4.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	gocv.io/x/gocv v0.35.0
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
---
+++ hash/hash.go
@@ -0,0 +1,86 @@
+package hash
+
+import (
+	"image"
+
+	"gocv.io/x/gocv"
+)
+
+const DCT_BLOCK_SIZE = 8
+
+func Phash(img *gocv.Mat) string {
+	// resize image to 32x32
+	newSize := image.Rectangle{
+		Min: image.Point{X: 0, Y: 0},
+		Max: image.Point{X: 32, Y: 32},
+	}
+	gocv.Resize(*img, img, newSize.Size(), 0, 0, gocv.InterpolationLinear)
+
+	// convert to grayscale
+	gocv.CvtColor(*img, img, gocv.ColorBGRToGray)
+
+	// use float32 to represent data
+	// https://stackoverflow.com/questions/22117267/how-to-convert-an-image-to-a-float-image-in-opencv
+	img.ConvertToWithParams(img, gocv.MatTypeCV32F, 1.0, 0.0)
+
+	gocv.DCT(*img, img, gocv.DftForward)
+
+	// dct_block = dct_block[:8, :8]
+	newSize.Max.X = DCT_BLOCK_SIZE
+	newSize.Max.Y = DCT_BLOCK_SIZE
+	dctBlock := img.Region(newSize)
+	dctAverage := float32(dctBlock.Mean().Val1)*float32(64) - dctBlock.GetFloatAt(0, 0)
+	dctAverage = dctAverage / float32(63)
+
+	// filter dctblock element < average to 0, else to 1 then add to output
+	var out string
+	for i := 0; i < DCT_BLOCK_SIZE; i++ {
+		for j := 0; j < DCT_BLOCK_SIZE; j++ {
+			if dctBlock.GetFloatAt(i, j) < dctAverage {
+				// dctBlock.SetFloatAt(i, j, float32(0))
+				out += "0"
+			} else {
+				// dctBlock.SetFloatAt(i, j, float32(1))
+				out += "1"
+			}
+		}
+	}
+
+	return out
+}
+
+func Dhash(img *gocv.Mat) string {
+	// resize image to 8x8
+	newSize := image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: image.Point{8, 8},
+	}
+	gocv.Resize(*img, img, newSize.Size(), 0, 0, gocv.InterpolationLinear)
+
+	// convert to grayscale
+	gocv.CvtColor(*img, img, gocv.ColorBGRToGray)
+
+	var out string
+	// TODO: rewrite this!!!!
+	for i := 0; i < 8; i++ {
+		if i == 0 || i == 7 {
+			for j := 1; j < 6; j++ {
+				if img.GetIntAt(i, j) < img.GetIntAt(i, j+1) {
+					out += "1"
+				} else {
+					out += "0"
+				}
+			}
+		} else {
+			for j := 0; j < 7; j++ {
+				if img.GetIntAt(i, j) < img.GetIntAt(i, j+1) {
+					out += "1"
+				} else {
+					out += "0"
+				}
+			}
+		}
+	}
+
+	return out
+}
