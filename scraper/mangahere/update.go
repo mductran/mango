ceb24f2d1b666aa0150dd1bab96677d41fff2954
mductran <ductran032118@gmail.com>
2024-03-10 22:19:09 +0700

    complete mangahere update functions

--- scraper/mangahere/update.go
+++ scraper/mangahere/update.go
@@ -1,13 +1,24 @@
 package scraper
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 var MONTHS = map[string]int{
 	"Jan": 1,
@@ -24,12 +35,6 @@ var MONTHS = map[string]int{
 	"Dec": 12,
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func Split(r rune) bool {
 	return r == ' ' || r == ','
 }
@@ -43,19 +48,19 @@ func ParseDate(s string) time.Time {
 		date, _ := strconv.Atoi(r[1])
 		year, _ := strconv.Atoi(r[2])
 
-		parsed := time.Date(year, time.Month(month), date, 0, 0, 0, 0, nil)
+		parsed := time.Date(year, time.Month(month), date, 0, 0, 0, 0, time.Local)
 
 		return parsed
 	}
 }
 
-func UpdateTitle(title *Manga) *[]Chapter {
+func UpdateTitle(title *mangahere.Manga) *[]mangahere.Chapter {
 	detailsPage, err := http.Get("https://mangahere.cc" + title.Url)
 	check(err)
 	detailsPageBody, err := goquery.NewDocumentFromReader(detailsPage.Body)
 	check(err)
 
-	var newChapterList []Chapter
+	var newChapterList []mangahere.Chapter
 
 	detailsPageBody.Find(".detail-main-list li a").EachWithBreak(func(i int, s *goquery.Selection) bool {
 
@@ -63,8 +68,8 @@ func UpdateTitle(title *Manga) *[]Chapter {
 
 		uploadTime, _ := node.Html()
 
-		if ParseDate(uploadTime).After(title.LastUpdate) {
-			newChapterList = append(newChapterList, *ParseChapter(s))
+		if ParseDate(uploadTime).After(title.LastUpdate) || title.LastUpdate.IsZero() {
+			newChapterList = append(newChapterList, ParseChapter(s))
 			return true
 		} else {
 			return false
@@ -72,4 +77,36 @@ func UpdateTitle(title *Manga) *[]Chapter {
 	})
 
 	return &newChapterList
+}
+
+func Update(col *mongo.Collection) {
+	cursor, err := col.Find(context.TODO(), bson.D{})
+	check(err)
+
+	var results []Manga
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		check(err)
+	}
+
+	for _, title := range results {
+		newChaptersList := UpdateTitle(&title)
+
+		fmt.Println(len(*newChaptersList))
+
+		update := bson.M{
+			"$addToSet": bson.M{
+				"chapters": bson.M{
+					"$each": *newChaptersList,
+				},
+			},
+			"$set": bson.M{
+				"last_update": time.Now(),
+			},
+		}
+
+		filter := bson.M{"title": title.Title}
+		result, err := col.UpdateOne(context.TODO(), filter, update)
+		check(err)
+		fmt.Println(result.ModifiedCount)
+	}
 }
